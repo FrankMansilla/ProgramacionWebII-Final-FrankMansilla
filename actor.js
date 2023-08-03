e9ab1f22dba2221f36b7adb625f7366d88d67452
Frankkmansilla <frankmansilla123a@gmail.com>
2023-08-03 17:08:19 -0300

    First Commit

---
+++ actor.js
@@ -0,0 +1,79 @@
+// Obtener los parámetros de la URL
+const urlParams = new URLSearchParams(window.location.search);
+const actorId = urlParams.get("id");
+const peliculasConocidas = decodeURIComponent(urlParams.get("known_for"));
+
+fetch(
+  `https://api.themoviedb.org/3/person/${actorId}?api_key=d13c9cff64cdd415463b6c8ead08c95e`
+)
+  .then((response) => response.json())
+  .then((data) => {
+    const actorContenedor = document.getElementById("actorContenedor");
+
+   
+    const imagenActor = document.createElement("img");
+    imagenActor.src = `https://image.tmdb.org/t/p/w500${data.profile_path}`;
+    imagenActor.alt = data.name;
+    actorContenedor.appendChild(imagenActor);
+
+    
+    const nombreActor = document.createElement("h3");
+    nombreActor.textContent = data.name;
+    nombreActor.classList.add("nombreActor")
+    actorContenedor.appendChild(nombreActor);
+
+    
+    const biografiaContenedor = document.getElementById("biografiaContenedor");
+    const biografiaActor = document.createElement("p");
+    biografiaActor.textContent = data.biography;
+    biografiaContenedor.appendChild(biografiaActor);
+
+    
+    const peliculasConocidasContainer = document.querySelector(".peliculasConocidasContainer");
+
+    
+    if (peliculasConocidas) {
+      const peliculasConocidasArray = peliculasConocidas.split(", ");
+      const peliculasConocidasValidas = peliculasConocidasArray.filter(
+        (pelicula) => pelicula.trim() !== "undefined" && pelicula.trim() !== ""
+      );
+
+      if (peliculasConocidasValidas.length > 0) {
+        peliculasConocidasValidas.forEach((pelicula) => {
+          const peliculaElemento = document.createElement("div");
+          peliculaElemento.classList.add("peliculaConocida");
+
+          // Mostrar el poster de la película
+          fetch(`https://api.themoviedb.org/3/search/movie?api_key=d13c9cff64cdd415463b6c8ead08c95e&query=${pelicula}`)
+            .then((response) => response.json())
+            .then((peliculaData) => {
+              if (peliculaData.results && peliculaData.results.length > 0) {
+                const posterPath = peliculaData.results[0].poster_path;
+                const posterURL = `https://image.tmdb.org/t/p/w200${posterPath}`;
+                const posterImagen = document.createElement("img");
+                posterImagen.src = posterURL;
+                peliculaElemento.appendChild(posterImagen);
+              }
+            })
+            .catch((error) => {
+              console.log("Error al cargar el poster del actor:", error);
+            });
+
+          // Mostrar el título de la película
+          const tituloPelicula = document.createElement("p");
+          tituloPelicula.textContent = pelicula;
+          peliculaElemento.appendChild(tituloPelicula);
+
+          // Agregar la película al contenedor de películas conocidas
+          peliculasConocidasContainer.appendChild(peliculaElemento);
+        });
+      } else {
+        peliculasConocidasContainer.textContent = "No hay actores conocidos disponibles.";
+      }
+    } else {
+      peliculasConocidasContainer.textContent = "No hay actores conocidos disponibles.";
+    }
+  })
+  .catch((error) => {
+    console.log("Error al cargar la información del actor:", error);
+  });
---
+++ app.js
@@ -0,0 +1,44 @@
+const cargarPopular = async () => {
+  try {
+    const respuesta = await fetch(
+      "https://api.themoviedb.org/3/person/popular?api_key=d13c9cff64cdd415463b6c8ead08c95e"
+    );
+
+    if (respuesta.status === 200) {
+      const datos = await respuesta.json();
+      let actorHTML = "";
+
+      datos.results.forEach((actor) => {
+        let peliculasConocidas = "";
+
+        actor.known_for.forEach((pelicula) => {
+          peliculasConocidas += pelicula.title + ", ";
+        });
+
+        peliculasConocidas = peliculasConocidas.slice(0, -2);
+
+        // Crear el elemento HTML y mostrarlo
+        actorHTML += `
+          <div class="actor">
+            <a href="actor.html?id=${actor.id}&known_for=${encodeURIComponent(peliculasConocidas)}">
+              <img class="poster" src="https://image.tmdb.org/t/p/w500${actor.profile_path}" alt="${actor.name}">
+              <h3 class="titulo">${actor.name}</h3>
+             
+            </a>
+          </div>`;
+      });
+
+      document.getElementById('contenedor').innerHTML = actorHTML;
+    } else if (respuesta.status === 401) {
+      console.log("los datos ingresados en la petición son incorrectos");
+    } else if (respuesta.status === 404) {
+      console.log("la peli que buscas no existe");
+    } else {
+      console.log("ocurrió un error desconocido");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+cargarPopular();
